0f4433e9d3729013e1a7aeacaee00ed0f608606b
Joshua Lowe <15jlow@allhallows.lancs.sch.uk>
2017-12-02 17:24:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,13 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '?help') {
-    	message.reply('Hi! What is a Raspberry Pi you ask? The Raspberry Pi is a series of small single-board computers developed in the United Kingdom by the Raspberry Pi Foundation to promote the teaching of basic computer science in schools and in developing countries');
+    	message.reply('Hi!\nWhat is a Raspberry Pi you ask?\nThe Raspberry Pi is a series of small single-board computers developed in the United Kingdom by the Raspberry Pi Foundation to promote the teaching of basic computer science in schools and in developing countries');
   	}
+});
+
+client.on("guildMemberAdd", (member) => {
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+  member.guild.channels.get("general").send(`"${member.user.username}" has joined this server`);
 });
 
 // THIS  MUST  BE  THIS  WAY
