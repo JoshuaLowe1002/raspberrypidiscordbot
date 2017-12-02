5d6bb72ffa6e3cc5ba84a4f19155d68946242049
Joshua Lowe <15jlow@allhallows.lancs.sch.uk>
2017-12-02 17:16:20 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '?ping') {
     	message.reply('pong');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'hello bot') {
+    if (message.content === '?hello') {
     	message.reply('Hi!');
   	}
 });
