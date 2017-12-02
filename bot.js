81dc1b86e4aa835f95ce12d7613adb806d0c5b00
Joshua Lowe <15jlow@allhallows.lancs.sch.uk>
2017-12-02 18:02:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,13 +41,9 @@ client.on('message', message => {
   }
 });
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('general', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
+const swearWords = ["brexit"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("Oh no you said a bad word!!!");
+  
+}
 
