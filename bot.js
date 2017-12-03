751a65de0cab3b3b3faf4587b7d4afda581e927f
Joshua Lowe <15jlow@allhallows.lancs.sch.uk>
2017-12-03 11:02:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,4 +42,10 @@ client.on('message', message => {
   }
 });
 
+const swearWords = ["brexit", "trump"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("Oh no you said a bad word!!!");
+  // Or just do message.delete();
+}
+
 
